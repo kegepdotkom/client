a0f8a202be2ea36ba7b62860c7425926d4f67369
Atras Tudhi <atras.r@gmail.com>
2018-12-07 16:05:03 +0700

    insyaallah final

--- components/show-content.js
+++ components/show-content.js
@@ -1,16 +1,22 @@
 Vue.component('show-content', {
     props: ['media'],
     template:`
-        <b-card v-for="content in media" title="Card Title"
-            img-src="content.url"
-            img-alt="Image"
-            img-top
-            tag="article"
-            style="max-width: 20rem;"
-            class="mb-2">
-            <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-        </b-card>
+        <div>
+            <b-card v-for="medias in media" :title="medias.title"
+                v-bind:img-src="medias.url"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2 m-3">
+                <p class="card-text">
+                    {{ medias.description }}
+                </p>
+                <div class="fb-share-button" 
+                    :data-href="medias.url" 
+                    data-layout="button_count">
+                </div>
+            </b-card>
+        </div>
     `
 })
